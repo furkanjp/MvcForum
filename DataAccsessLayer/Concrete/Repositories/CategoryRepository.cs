7dc86c4c281de40a88ea733bac0990251e101127
Furkan Demirtaş <demirtasfurkan911@gmail.com>
2021-04-21 02:04:21 +0300

    Interface ve Crud işlemleri tanımlandı.

---
+++ DataAccsessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccsessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccsessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+        DbSet<Category> _object;
+
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p); //parametreden aldığın değerleri ekle.
+            c.SaveChanges(); //Aldığın değerleri kaydet.
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList(); 
+        }
+
+        public void Update(Category p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
+/*
+ * To List
+ * Add
+ * Remover 
+ * Find
+ */
